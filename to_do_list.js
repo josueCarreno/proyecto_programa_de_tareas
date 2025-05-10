ff213fbf0d32309aacd357ec8a7e8faac0ca3fa7
josueCarreno <150408018+josueCarreno@users.noreply.github.com>
2025-05-10 17:29:28 -0400

    celaboración de comentarios
    culminación de código, en versión 1.0

--- to_do_list.js
+++ to_do_list.js
@@ -15,8 +15,9 @@ function empezar() {
 
 /** 
  *@function pedirOpcion
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
  *@description función que muestra el menú, las posibles opciones a escojer representadas del 1 al 4
- *@author Josue Carreño
 */
 function pedirOpcion(listado = [], opcion = "") {
   opcion = prompt("MENÚ: \n\nSeleccione alguna de las siguientes opciones por medio de un número: \n\n1.  Agregar Tarea \n2.  Eliminar Tarea \n3.  Marcar Tarea como Realizada \n4.  Listar Tareas");
@@ -30,6 +31,12 @@ function pedirOpcion(listado = [], opcion = "") {
   }
 }
 
+/** 
+ *@function evaluarArreglo
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
+ *@description función que evalúa si el arreglo está vacío, en caso de que se quiera realizar una consulta
+*/
 function evaluarArreglo(listado, opcion) {
   if (listado.length === 0) {
     alert("¡No existe ninguna tarea!");
@@ -40,14 +47,12 @@ function evaluarArreglo(listado, opcion) {
   } 
 }
 
-
-function agregarTarea(listado, opcion){
-  let tarea = prompt("Por favor, Ingrese el nombre de la nueva tarea");
-  listado.push({deber:tarea.toLowerCase()});
-  alert("¡Se ha registrado su Tarea con Exito!");
-  repetirProceso(listado, opcion);
-}
-
+/** 
+ *@function repetirProceso
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
+ *@description Pregunta al usuario si desea repetir el proceso anterior
+*/
 function repetirProceso(listado, opcion) {
   let repetir = prompt("¿Desea repetir el proceso anterior? S/N");
   repetir.toLowerCase();
@@ -65,6 +70,26 @@ function repetirProceso(listado, opcion) {
   }
 }
 
+/** 
+ *@function agregarTarea
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
+ *@description función que agrega a Listado un objeto con la propiedad: deber. El valor a asignar es la variable tarea pedida al usuario por medio de un promt
+*/
+function agregarTarea(listado, opcion){
+  let tarea = prompt("Por favor, Ingrese el nombre de la nueva tarea");
+  listado.push({deber:tarea.toLowerCase()});
+  alert("¡Se ha registrado su Tarea con Exito!");
+  repetirProceso(listado, opcion);
+}
+
+/** 
+ *@function eliminarTarea
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
+ *@param {""} tarea - String que contiene el valor de la tarea que se desea eliminar
+ *@description Elimina el objeto en el que se encuentra la tarea a eliminar escrita por el usuario anteriormente
+*/
 function eliminarTarea(listado, opcion, tarea) {
   listado.map(
     (lista) => {
@@ -79,7 +104,13 @@ function eliminarTarea(listado, opcion, tarea) {
   repetirProceso(listado, opcion);
 }  
 
-
+/** 
+ *@function marcarCompletado
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
+ *@param {""} tarea - String que contiene el valor de la tarea que se desea marcar como completada
+ *@description si la tarea existe entones añade una nueva propiedad al objeto: completado = true
+*/
 function marcarCompletado(listado, opcion, tarea) {
   listado.map(
     (lista) => {
@@ -94,11 +125,12 @@ function marcarCompletado(listado, opcion, tarea) {
   repetirProceso(listado, opcion);
 }
 
-function listarTareas(listado, imprimirListado){
-  alert("Su lista de Tareas: \n" + imprimirListado);
-  pedirOpcion(listado);
-}
-
+/** 
+ *@function imprimir
+ *@param {[]} listado - Arreglo donde se almacenan los objetos. Cada objeto es una tarea
+ *@param {""} opcion - String el cual proporciona la petición del usuario para que se realice un proceso 
+ *@description Generae imprime un listado de todas las tareas, también de las pendientes y las realizadas. también evalúa que cada listado contenga algún valor antes de mostrarse de lo contrario avisa que no existe tarea con las caracteristicas proporcionadas
+*/
 function imprimir(listado, opcion) {
   let imprimirListado = "";
   let imprimirCompletados = "";
@@ -113,32 +145,39 @@ function imprimir(listado, opcion) {
       }
     }
   );
-  if (opcion == "2") {
-    let tarea = prompt("Escriba alguna de las tareas siguientes que desee eliminar \n\n" + imprimirListado); 
-    eliminarTarea(listado, opcion, tarea.toLowerCase());
-  }
-  if (opcion == "3") {
-    let tarea = prompt("Escriba alguna de las tareas siguientes que ha completado \n\n" + imprimirPendientes);
-    marcarCompletado(listado, opcion, tarea.toLowerCase());
-  }
-  if (opcion == "4") {
-    let tarea = prompt("Escoja que listado de  tareas desea ver: \n1.  Tareas Completadas \n2.  Tareas Incompletas \n3.  Todas las Tareas");
-    if (tarea == "1" && imprimirCompletados.length == 0) {
-      alert("¡No tiene Tareas Completadas!");
-    } 
-    else if (tarea == "1" && imprimirCompletados.length > 0) {
-      alert("Su lista de Tareas Realizadas: \n" + imprimirCompletados);
-    }
-    else if (tarea == "2" && imprimirPendientes.length == 0) {
-      alert("¡No tiene Tareas Icompletas!");
-    } 
-    else if (tarea == "2" && imprimirPendientes.length > 0) {
-      alert("Su lista de Tareas pendientes: \n" + imprimirPendientes);
-    }
-    else if (tarea == "3") {
-        alert("Su lista de Tareas: \n" + imprimirListado);
-    }
-    repetirProceso(listado, opcion);
+  let tarea = "";
+  switch (opcion){
+    case "2":
+      tarea = prompt("Escriba alguna de las tareas siguientes que desee eliminar \n" + imprimirListado); 
+      eliminarTarea(listado, opcion, tarea.toLowerCase());
+      break;
+    case "3":
+      tarea = prompt("Escriba alguna de las tareas siguientes que ha completado \n" + imprimirPendientes);
+      marcarCompletado(listado, opcion, tarea.toLowerCase());
+    case "4":
+      tarea = prompt("Escoja que listado de  tareas desea ver: \n1.  Tareas Completadas \n2.  Tareas Incompletas \n3.  Todas las Tareas");
+      switch (tarea){
+        case "1":
+          if (imprimirCompletados.length > 0) {
+            alert("Su lista de Tareas Realizadas: \n" + imprimirCompletados);
+          }
+          else {
+            alert("¡No tiene Tareas Completadas!");
+          }
+          break;
+        case "2":
+          if (imprimirPendientes.length > 0) {
+            alert("Su lista de Tareas pendientes: \n" + imprimirPendientes);
+          }
+          else {
+            alert("¡No tiene Tareas Incompletas!");
+          }
+          break;
+        case "3":
+          alert("Su lista de Tareas: \n" + imprimirListado);
+          break;
+      }
+      repetirProceso(listado, opcion);
   } 
 }
 
